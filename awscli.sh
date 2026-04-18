3c3bf2d5f540b9686fea18e7374497ffcfa50aee
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-18 22:45:35 +0530

    Create awscli.sh

---
+++ awscli.sh
@@ -0,0 +1,46 @@
+if aws --version; then
+
+echo "aws cli installed"
+
+else
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+ID=$(cat /etc/os-release | grep -w ID | cut -d "=" -f2 |tr -d '"')
+
+                                               
+function_ubuntu(){   
+sudo apt update -y && sudo apt-get install unzip -y
+
+}
+
+
+function_redhat(){    
+
+sudo yum update -y && sudo yum install unzip -y
+
+}
+
+if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then          
+
+  function_ubuntu
+
+
+elif [ "$ID" = "rhel" ] || [ "$ID" = "amzn" ]; then   
+                                                                   
+  function_redhat                                                                               
+
+else
+
+echo "notfound"          
+
+
+  fi 
+
+  fi
+  
+unzip awscliv2.zip 
+sudo ./aws/install 
+
+
+  
